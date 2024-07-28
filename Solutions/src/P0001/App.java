dfd2ca26ae877355585c851630127d7f078e2018
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-07-28 02:44:13 +0200

    Another solution for 0001 using hash map

--- Solutions/src/P0001/App.java
+++ Solutions/src/P0001/App.java
@@ -1,7 +1,7 @@
 package P0001;
 public class App {
     public static void main(String[] args) throws Exception {
-        Solution s = new Solution();
+        Solution1 s = new Solution1();
         int[] nums = {2,7,11,15};
         int target = 9;
         int[] result = s.twoSum(nums, target);
