51cf919e4cc72f76f8441d87151cc670f8919bea
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-08-09 23:02:49 +0200

    problem 0024 solved

---
+++ Solutions/src/P0024/Solution.java
@@ -0,0 +1,18 @@
+package P0024;
+
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode current = dummy;
+        while(current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+}
